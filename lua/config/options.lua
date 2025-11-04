f6973d9e12bc311edd0761eca42e2afefc328007
minmin2104 <aminkafri0045@gmail.com>
2025-11-04 09:43:45 +0800

    add undotree, remap and add more options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,10 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
+vim.opt.undofile = true
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.wrap = false
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,3 +3,25 @@ vim.keymap.set("v", "<leader>x", ":lua<cr>")
 vim.keymap.set("n", "<leader><leader>x", ":source %<cr>")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set('i', '<C-c>', '<Esc>')
+vim.keymap.set('n', "<M-j>", '<cmd>cnext<CR>zz')
+vim.keymap.set('n', "<M-k>", '<cmd>cprev<CR>zz')
+
+vim.keymap.set('v', 'J', ":m '>+1'<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2'<CR>gv=gv")
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
+vim.keymap.set('n', '<leader>cx', ':!chmod +x %<cr>');
+
+vim.keymap.set('n', '<leader>cgn', '*N_cgn');
+
+vim.keymap.set('n', '<M-=>', '<C-w>+')
+vim.keymap.set('n', '<M-->', '<C-w>-')
+vim.keymap.set('n', '<M-,>', '<C-w><')
+vim.keymap.set('n', '<M-.>', '<C-w>>')
