147b9708e412a6525cd3b4d0c2d6063173c35f47
minmin2104 <aminkafri0045@gmail.com>
2025-11-04 09:16:23 +0800

    add lazy, tender colorscheme and telescope

---
+++ lua/config/remap.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader>x", ":.lua<cr>")
+vim.keymap.set("v", "<leader>x", ":lua<cr>")
+vim.keymap.set("n", "<leader><leader>x", ":source %<cr>")
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	-- or                              , branch = '0.1.x',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>fn', function()
+			builtin.find_files({
+				cwd = vim.fn.stdpath("config")
+			})
+		end, { desc = 'Telescope find neovim config files' })
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	end
+}
